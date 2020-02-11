ffcfeb142329e48c03e58e403f665336aabbc483
Kevin Devine-Jones <kevin.jones215@gmail.com>
2020-02-10 22:00:50 -0500

    changes database from json to js

---
+++ db/db.js
@@ -0,0 +1,5 @@
+const notes = [
+    { title:"Test Title",text:"Test text" }
+];
+
+module.exports = notes;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,4 @@
-const fs = require('fs');
-const notes = require('../db/db.json');
+const notes = require('../db/db.js');
 
 module.exports = function(app){
     app.get("/api/notes", (req,res) => {
@@ -8,19 +7,7 @@ module.exports = function(app){
 
     app.post('/api/notes', (req,res) => {
         const newNote = req.body;
-        // console.log(JSON.stringify(newNote));
-        // something here is broken. Fix me please.
-        // possibly that the resolve to our promise is firing before the readFile is done.
-        // may just need to fiddle around with the order.
-        return new Promise((resolve) => {
-            let notePad = '';
-            fs.readFile(__dirname + "/../db/db.json", (err,data) => {
-                if (err) console.log(err);
-                notePad = data;
-            });
-            resolve(notePad);
-        }).then(function(result){
-            console.log(result);
-        });
+        notes.push(newNote);
+        console.log(notes);
     });
 }
