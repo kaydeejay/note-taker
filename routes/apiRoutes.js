d12e2e3b6eafc3b64d2c75ebc3e74279977eb692
Kevin Devine-Jones <kevin.jones215@gmail.com>
2020-02-10 20:22:36 -0500

    starts building the post route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,23 @@
+const fs = require('fs');
 const notes = require('../db/db.json');
 
 module.exports = function(app){
     app.get("/api/notes", (req,res) => {
         res.json(notes);
+    });
+
+    app.post('/api/notes', (req,res) => {
+        const newNote = req.body;
+        // console.log(JSON.stringify(newNote));
+        return new Promise((resolve) => {
+            let notePad = '';
+            fs.readFile(__dirname + "/../db/db.json", (err,data) => {
+                if (err) console.log(err);
+                notePad = data;
+            });
+            resolve(notePad);
+        }).then(function(result){
+            console.log(result);
+        });
     });
 }
