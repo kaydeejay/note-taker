679f6f139e853a2ffc646c3ce45544d42b5826da
Kevin Devine-Jones <kevin.jones215@gmail.com>
2020-02-10 20:23:59 -0500

    adds to-do comments to apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,6 +9,9 @@ module.exports = function(app){
     app.post('/api/notes', (req,res) => {
         const newNote = req.body;
         // console.log(JSON.stringify(newNote));
+        // something here is broken. Fix me please.
+        // possibly that the resolve to our promise is firing before the readFile is done.
+        // may just need to fiddle around with the order.
         return new Promise((resolve) => {
             let notePad = '';
             fs.readFile(__dirname + "/../db/db.json", (err,data) => {
