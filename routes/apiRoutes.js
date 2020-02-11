0cd2585bc392f332da02d9394208d62f5b8be91b
Kevin Devine-Jones <kevin.jones215@gmail.com>
2020-02-11 00:04:31 -0500

    tweaks to post routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,18 @@
 const notes = require('../db/db.js');
 
 module.exports = function(app){
+
+    // ======== GET routes ========
     app.get("/api/notes", (req,res) => {
         res.json(notes);
     });
 
+    // ======== POST routes ========
     app.post('/api/notes', (req,res) => {
         const newNote = req.body;
+        newNote.id = notes.length;
         notes.push(newNote);
         console.log(notes);
+        res.json(notes);
     });
 }
