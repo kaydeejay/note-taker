89cdf6cc155f25ea172f383e76995b76094813f0
Kevin Devine-Jones <kevin.jones215@gmail.com>
2020-02-12 15:30:35 -0500

    re-working create path

--- db/db.js
+++ db/db.js
@@ -1,13 +1,17 @@
-const notes = {};
+const notes = [];
 
 module.exports = {
     create: (req,res) => {
         const newNote = req.body;
         newNote.id = Object.keys(notes).length + 1;
-        newNote.getId = () => {return this.id};
-        notes[`note${newNote.id}`] = newNote;
-        console.log(`Notes after post:\n${JSON.stringify(notes)}`);
-        res.end(`${JSON.stringify(newNote)}`);
+        notes.push(newNote);
+        // console.log(notes);
+        // re-assign id's based on position in array
+        // notes.forEach(el => console.log(el.id));
+        for (let i = 0; i < notes.length; i++) {
+            // console.log(notes[i].id);
+            notes[i].id = i;
+        }
     },
     findAll: (req,res) => {
         console.log(`All Notes:\n${JSON.stringify(notes)}`);
@@ -20,3 +24,19 @@ module.exports = {
         res.end(`Deleted Note\n${JSON.stringify(deleteNote)}\nNotes Remaining:\n${JSON.stringify(notes)}`);
     }
 };
+
+//function graveyard
+
+// create: (req,res) => {
+//     const newNote = req.body;
+//     newNote.id = Object.keys(notes).length + 1;
+//     newNote.getId = () => {return this};
+//     notes[`note${newNote.getId}`] = newNote;
+//     //find the largest id in notes
+//     for (note in notes) {
+//         let thisId = notes[note].getId();
+//         console.log(thisId);
+//     }
+//     console.log(`Notes after post:\n${JSON.stringify(notes)}`);
+//     res.end(`${JSON.stringify(newNote)}`);
+// },
